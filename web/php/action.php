ed60637f385ed0b5ca2f9557fa872b4a9eda4af5
Michael DeGraw <mkdegraw@gmail.com>
2016-12-14 11:52:14 -0600

    test

---
+++ web/php/action.php
@@ -0,0 +1,21 @@
+<?php
+
+  ob_start();
+
+  // Get crap from post
+  $action = htmlspecialchars($_POST['action']);
+  $username = htmlspecialchars($_POST['username']);
+  $password = htmlspecialchars($_POST['password']);
+
+  if (strcmp($action, "login")){
+    echo "Selected login";
+  } elseif (strcmp($action, "new")){
+    echo "Selected new";
+  } elseif (strcmp($action, "reset")){
+    echo "Selected reset";
+  } else {
+    // ERROR
+    echo "ERROR";
+  }
+
+?>
