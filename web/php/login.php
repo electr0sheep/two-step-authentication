aa449db764900680b2b33fc3fa86d5f53d1a2116
Michael DeGraw <mkdegraw@gmail.com>
2016-12-16 10:12:36 -0600

    test

--- web/php/login.php
+++ web/php/login.php
@@ -1,4 +1,4 @@
-<!-- <?php
+<?php
 
   ob_start();
 
@@ -69,51 +69,53 @@
 
   //sendResponse("Login successful", true);
 
+  echo("
+  <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+  <html>
+    <title>My jQuery JSON Web Page</title>
+    <head>
+      <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+      <script type="text/javascript">
+
+        JSONTest = function() {
+
+          var resultDiv = $("#resultDivContainer");
+
+          $.ajax({
+          url: "https://example.com/api/",
+          type: "POST",
+          data: { apiKey: "23462", method: "example", ip: "208.74.35.5" },
+          dataType: "json",
+          success: function (result) {
+              switch (result) {
+                  case true:
+                      processResponse(result);
+                      break;
+                  default:
+                      resultDiv.html(result);
+              }
+          },
+          error: function (xhr, ajaxOptions, thrownError) {
+            alert(xhr.status);
+            alert(thrownError);
+          }
+        });
+      };
+
+      </script>
+    </head>
+    <body>
+
+      <h1>My jQuery JSON Web Page</h1>
+
+      <div id="resultDivContainer"></div>
+
+      <button type="button" onclick="JSONTest()">JSON</button>
+
+    </body>
+  </html>
+  ");
+
   mysqli_close($conn);
 
-?> -->
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
-<title>My jQuery JSON Web Page</title>
-<head>
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-<script type="text/javascript">
-
-JSONTest = function() {
-
-    var resultDiv = $("#resultDivContainer");
-
-    $.ajax({
-        url: "https://example.com/api/",
-        type: "POST",
-        data: { apiKey: "23462", method: "example", ip: "208.74.35.5" },
-        dataType: "json",
-        success: function (result) {
-            switch (result) {
-                case true:
-                    processResponse(result);
-                    break;
-                default:
-                    resultDiv.html(result);
-            }
-        },
-        error: function (xhr, ajaxOptions, thrownError) {
-        alert(xhr.status);
-        alert(thrownError);
-        }
-    });
-};
-
-</script>
-</head>
-<body>
-
-<h1>My jQuery JSON Web Page</h1>
-
-<div id="resultDivContainer"></div>
-
-<button type="button" onclick="JSONTest()">JSON</button>
-
-</body>
-</html>
+?>
