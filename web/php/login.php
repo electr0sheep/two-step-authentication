e9424663cb8d3bd217d5321351aff723d8281cf0
Michael DeGraw <mkdegraw@gmail.com>
2016-12-20 10:47:31 -0600

    check to see two-step set up

--- web/php/login.php
+++ web/php/login.php
@@ -43,42 +43,51 @@
   $storedpassword = bin2hex($row["password"]);
   if ($storedpassword != $encryptedpassword){
     sendResponse("Invalid password", false);
+  }
+
+  // check to see if two-step authentication has been set up
+  $sql = "SELECT fcm_token FROM users WHERE name = '{$username}'";
+  $result = $conn->query($sql);
+  if ($result->num_rows == 0){
+    sendResponse("Two-authentication has not been set up", false);
+  }
+
+  // send the two-step authentication notification to android device
+
+  $curl = curl_init();
+
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://fcm.googleapis.com/fcm/send",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => "{\n
+      \"notification\" : {\n
+      \"title\" : \"Authentication requested\",\n
+      },\n
+      \"to\" : \"eW1zaAGunwE:APA91bEhIm9g8Kst_UgPAaPPhmcBfoI1JfY1Q87CHA7GR1D-HeMyut9ZSJxz4tLa0c2tE42w2rAyhZ4nduOPiyPgQ7FeYewPcggzJcHnnEd_O8267TWpddjAR9PSZlHa-Xs6PvPL--1_\"\n
+      \"priority\" :
+      \"high\"\n
+      }",
+    CURLOPT_HTTPHEADER => array(
+      "authorization: key=AAAAlidsJ90:APA91bHgn-GGtJaesrCRmecBh77KaP8LqdBkRW9ng8spywONeAVSmJf9TY7N4Qw7SShyWCKVhIxWxtxSoQC7c4kFuZGQguibnAtKBZlttWd7LJIOFv9e_FqgDXRwzrtiruVXqftDvZpZyTqGGMDS4jHbxoasYLx43w",
+      "cache-control: no-cache",
+      "content-type: application/json"
+    ),
+  ));
+
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
+
+  curl_close($curl);
+
+  if ($err) {
+    echo "cURL Error #:" . $err;
   } else {
-    $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => "https://fcm.googleapis.com/fcm/send",
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => "",
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 30,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => "POST",
-      CURLOPT_POSTFIELDS => "{\n
-        \"notification\" : {\n
-        \"title\" : \"Authentication requested\",\n
-        },\n
-        \"to\" : \"eW1zaAGunwE:APA91bEhIm9g8Kst_UgPAaPPhmcBfoI1JfY1Q87CHA7GR1D-HeMyut9ZSJxz4tLa0c2tE42w2rAyhZ4nduOPiyPgQ7FeYewPcggzJcHnnEd_O8267TWpddjAR9PSZlHa-Xs6PvPL--1_\"\n
-        \"priority\" :
-        \"high\"\n
-        }",
-      CURLOPT_HTTPHEADER => array(
-        "authorization: key=AAAAlidsJ90:APA91bHgn-GGtJaesrCRmecBh77KaP8LqdBkRW9ng8spywONeAVSmJf9TY7N4Qw7SShyWCKVhIxWxtxSoQC7c4kFuZGQguibnAtKBZlttWd7LJIOFv9e_FqgDXRwzrtiruVXqftDvZpZyTqGGMDS4jHbxoasYLx43w",
-        "cache-control: no-cache",
-        "content-type: application/json"
-      ),
-    ));
-
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
-
-    curl_close($curl);
-
-    if ($err) {
-      echo "cURL Error #:" . $err;
-    } else {
-      echo $response;
-    }
+    echo $response;
   }
 
   //sendResponse($storedpassword."   also   ".$encryptedpassword, false);
