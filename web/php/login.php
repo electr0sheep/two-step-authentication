d389f05260ac05ece351ad9eddc14f703fd21d29
Michael DeGraw <mkdegraw@gmail.com>
2016-12-20 10:42:21 -0600

    test

--- web/php/login.php
+++ web/php/login.php
@@ -56,8 +56,7 @@
       CURLOPT_CUSTOMREQUEST => "POST",
       CURLOPT_POSTFIELDS => "{\n
         \"notification\" : {\n
-        \"body\" : \"Goku\",\n
-        \"title\" : \"Over 9000\",\n
+        \"title\" : \"Authentication requested\",\n
         },\n
         \"to\" : \"eW1zaAGunwE:APA91bEhIm9g8Kst_UgPAaPPhmcBfoI1JfY1Q87CHA7GR1D-HeMyut9ZSJxz4tLa0c2tE42w2rAyhZ4nduOPiyPgQ7FeYewPcggzJcHnnEd_O8267TWpddjAR9PSZlHa-Xs6PvPL--1_\"\n
         \"priority\" :
@@ -80,51 +79,6 @@
     } else {
       echo $response;
     }
-    // $request = new HttpRequest();
-    // $request->setUrl('https://fcm.googleapis.com/fcm/send');
-    // $request->setMethod(HTTP_METH_POST);
-    //
-    // $request->setHeaders(array(
-    //   'cache-control' => 'no-cache',
-    //   'content-type' => 'application/json',
-    //   'authorization' => 'key=AAAAlidsJ90:APA91bHgn-GGtJaesrCRmecBh77KaP8LqdBkRW9ng8spywONeAVSmJf9TY7N4Qw7SShyWCKVhIxWxtxSoQC7c4kFuZGQguibnAtKBZlttWd7LJIOFv9e_FqgDXRwzrtiruVXqftDvZpZyTqGGMDS4jHbxoasYLx43w'
-    // ));
-    //
-    // $request->setBody('{
-    //   "data" : {
-    //   "name" : "Goku",
-    //   "power_level" : "Over 9000",
-    //   "fighting_skill" : "excellent"
-    // },
-    //   "to" : "fTF_RuYQ_VM:APA91bGrcl7MfoCHuGvfrlNsWzqQn_uImPeiranNbgGS8rR2N8im6B76IPveNECThSKgiiSAe6HDYDntWMsCjn9zBsQDWxoQiYI59gdTSla1TGkGV3p1LOqNGAXE0OVqxBafB9CoT_gN"
-    // }');
-    //
-    // try {
-    //   $response = $request->send();
-    //
-    //   echo $response->getBody();
-    // } catch (HttpException $ex) {
-    //   echo $ex;
-    // }
-    // $url = 'https://fcm.googleapis.com/fcm/send';
-    // $data = array('score' => '5x1', 'time' => '15:10');
-    //
-    // // use key 'http' even if you send the request to https://...
-    // $options = array(
-    //   'http' => array(
-    //     'header'  =>  "Content-type: application/json\r\n" .
-    //                   "Authorization: key=AAAAlidsJ90:APA91bHgn-GGtJaesrCRmecBh77KaP8LqdBkRW9ng8spywONeAVSmJf9TY7N4Qw7SShyWCKVhIxWxtxSoQC7c4kFuZGQguibnAtKBZlttWd7LJIOFv9e_FqgDXRwzrtiruVXqftDvZpZyTqGGMDS4jHbxoasYLx43w\r\n",
-    //     'method'  => 'POST',
-    //     'data' => http_build_query($data),
-    //     'to' => $fcm
-    //   )
-    // );
-    // $context  = stream_context_create($options);
-    // $result = file_get_contents($url, false, $context);
-    // if ($result === FALSE) { /* Handle error */ }
-    //
-    // var_dump($result);
-    //sendResponse("Login successful", true);
   }
 
   //sendResponse($storedpassword."   also   ".$encryptedpassword, false);
