b4e203ae159752cd777e61266c7b3ae4c7b5be29
Michael DeGraw <mkdegraw@gmail.com>
2016-12-20 09:36:51 -0600

    changed to notification message

--- web/php/login.php
+++ web/php/login.php
@@ -55,10 +55,9 @@
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
       CURLOPT_CUSTOMREQUEST => "POST",
       CURLOPT_POSTFIELDS => "{\n
-        \"data\" : {\n
-          \"name\" : \"Goku\",\n
-          \"power_level\" : \"Over 9000\",\n
-          \"fighting_skill\" : \"excellent\"\n
+        \"notification\" : {\n
+          \"body\" : \"Goku\",\n
+          \"title\" : \"Over 9000\",\n
           },\n
           \"to\" : \"eW1zaAGunwE:APA91bEhIm9g8Kst_UgPAaPPhmcBfoI1JfY1Q87CHA7GR1D-HeMyut9ZSJxz4tLa0c2tE42w2rAyhZ4nduOPiyPgQ7FeYewPcggzJcHnnEd_O8267TWpddjAR9PSZlHa-Xs6PvPL--1_\"\n
           }",
