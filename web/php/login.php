1cb81f60b667f7a6d810d918bcb6b379b4b8bd58
Michael DeGraw <mkdegraw@gmail.com>
2016-12-21 11:49:09 -0600

    test

--- web/php/login.php
+++ web/php/login.php
@@ -70,7 +70,7 @@
       \"notification\" : {\n
         \"title\" : \"Authentication requested\",\n
         \"icon\" : \"ic_stat_name\",\n
-        \"click_action\" : \"MAIN\",\n
+        \"click_action\" : \"android.intent.action.MAIN\",\n
       },\n
       \"to\" : \"{$fcm_token}\"\n
       \"priority\" : \"high\"\n
