63d2beabc8160c244757885ace9385ead3b27846
Michael DeGraw <mkdegraw@gmail.com>
2016-12-21 11:48:10 -0600

    test

--- web/php/login.php
+++ web/php/login.php
@@ -70,7 +70,7 @@
       \"notification\" : {\n
         \"title\" : \"Authentication requested\",\n
         \"icon\" : \"ic_stat_name\",\n
-        \"click_action\" : \"AUTHENTICATE\",\n
+        \"click_action\" : \"MAIN\",\n
       },\n
       \"to\" : \"{$fcm_token}\"\n
       \"priority\" : \"high\"\n
