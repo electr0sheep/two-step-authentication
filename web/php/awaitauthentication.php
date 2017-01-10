096ca89ec33271f4ed5c837df7a24ea853f50633
Michael DeGraw <mkdegraw@gmail.com>
2017-01-10 16:29:17 -0600

    test

--- web/php/awaitauthentication.php
+++ web/php/awaitauthentication.php
@@ -27,7 +27,7 @@ Awaiting authentication...
   function checkDatabase() {
     xhr.open("POST", "/php/checkauthenticated.php", true);
     xhr.onreadystatechange = processRequest;
-    xhr.setRequestHeader("Content-type", "application/JSON");
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr.send("username=michael");
   }
 
