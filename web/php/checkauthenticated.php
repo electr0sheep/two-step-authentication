f4aa169de3abcdf4afe50f7b5b4aa5986c19706b
Michael DeGraw <mkdegraw@gmail.com>
2017-01-10 15:53:24 -0600

    test

--- web/php/checkauthenticated.php
+++ web/php/checkauthenticated.php
@@ -22,8 +22,8 @@
   $authenticated = $row["pending_authentication"];
 
   if ($authenticated == "0") {
-    sendResponse("Success", true);
+    sendResponse($authenticated, true);
   } else {
-    sendResponse("Nope", false);
+    sendResponse($authenticated, false);
   }
 ?>
