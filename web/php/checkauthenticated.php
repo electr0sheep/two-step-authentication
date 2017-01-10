e8869b4289d1fd9460f01c7803a7c697bab01ba8
Michael DeGraw <mkdegraw@gmail.com>
2017-01-10 16:59:01 -0600

    test

--- web/php/checkauthenticated.php
+++ web/php/checkauthenticated.php
@@ -16,7 +16,7 @@
   }
 
   // Check status of pending_authentication
-  $sql = "SELECT pending_authentication FROM users WHERE name = '{$username}'";
+  $sql = "SELECT * FROM users WHERE name = '{$username}'";
   $result = $conn->query($sql);
   if ($result->num_rows == 0){
     sendResponse("no rows", false);
