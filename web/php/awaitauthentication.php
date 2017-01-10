e22707c0be262c91b1b12f14ba2274de8e70f704
Michael DeGraw <mkdegraw@gmail.com>
2017-01-10 15:29:27 -0600

    test

--- web/php/awaitauthentication.php
+++ web/php/awaitauthentication.php
@@ -6,24 +6,31 @@ Awaiting authentication...
 <script>
   var interval = null;
   var formData = {username: "<?php echo $username ?>"};
+  var xhr = new XMLHttpRequest();
 
-  function processRequest(e) {
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  async function processRequest(e) {
     if (xhr.readyState == 4 && xhr.status == 200) {
       var response = JSON.parse(xhr.responseText);
       if (response.result == true){
         alert("success");
         clearInterval(interval);
+      } else {
+        alert("nope");
+        await sleep(2000);
+        checkDatabase();
       }
     }
   }
 
-
-  var checkDatabase = function() {
-    var xhr = new XMLHttpRequest();
+  function checkDatabase() {
     xhr.open('POST', "/php/checkauthenticated.php", true);
     xhr.onreadystatechange = processRequest;
     xhr.send();
   }
-  interval = setInterval(checkDatabase, 2000);
-  // checkDatabase(); // start the process...
+
+  checkDatabase(); // start the process...
 </script>
