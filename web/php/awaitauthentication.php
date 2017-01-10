c9c1ccdfc9ad386533ada0bad2512376cb729f72
Michael DeGraw <mkdegraw@gmail.com>
2017-01-10 14:20:18 -0600

    test

--- web/php/awaitauthentication.php
+++ web/php/awaitauthentication.php
@@ -1,11 +1,25 @@
 <?php
   $username = htmlspecialchars($_POST['username']);
-?>
 
-Awaiting authentication...
-<script>
-  setInterval(function checkDatabase() {
-    load("checkauthenticated.php", {username:<?php echo($username) ?>});
-  }, 1000);
-  checkDatabase(); // start the process...
-</script>
+  //Connect to the database
+  include 'serverinfo.php';
+  include 'superuser.php';
+
+  // Create connection
+  $conn = new mysqli($servername, $superusername, $superuserpassword, $databasename);
+
+  // Check connection
+  if ($conn->connect_error) {
+    sendResponse('Connection failed: '.$conn->connect_error, false);
+  }
+
+  $sec = 1;
+  while ($sec <= 30) {
+    // Check status of pending_authentication
+    $sql = "SELECT pending_authentication FROM users WHERE name = '{$username}'";
+    $result = $conn->query($sql);
+    $row = $result->fetch_array(MYSQLI_ASSOC);
+    $authenticated = $row["pending_authentication"];
+    echo($authenticated);
+  }
+?>
