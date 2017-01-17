d29d728120dc92037ff2f5382f914115e6d670b0
Michael DeGraw <mkdegraw@gmail.com>
2017-01-17 16:59:23 -0600

    test

--- web/scripts/custom.js
+++ web/scripts/custom.js
@@ -1,15 +1,37 @@
+var xhr = new XMLHttpRequest();
+
 function loginButtonOnClick() {
-  alert("You clicked the login button");
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  xhr.open('POST', "/php/login.php", true);
+  xhr.onreadystatechange = processRequest;
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.send("username=" + username + "&password=" + password);
 }
 
 function newUserButtonOnClick() {
-  alert("You clicked the new user button");
+  // show new user modal
+  alert("You clicked the new user modal");
 }
 
 function resetpwButtonOnClick() {
+  var username = document.getElementById('username').value;
+  // show reset pw modal
   alert("You clicked the reset pw button");
 }
 
 function dlapkButtonOnClick() {
+  // download file
   alert("You clicked the dl button");
+}
+
+async function processRequest(e) {
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    var response = JSON.parse(xhr.responseText);
+    if (response.result == true){
+      alert("Seems like it worked")
+    } else {
+      alert("Something went wrong");
+    }
+  }
 }
