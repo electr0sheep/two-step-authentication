cde40cf293aa0f77caf847646ec40a272fb69f7c
Michael DeGraw <mkdegraw@gmail.com>
2017-01-17 16:39:43 -0600

    test

--- web/scripts/custom.js
+++ web/scripts/custom.js
@@ -1,5 +1,15 @@
-// $(document).ready(function(){
-//     $("#newUserButton").click(function(){
-//         $("#myModal").modal();
-//     });
-// });
+function loginButtonOnClick() {
+  alert("You clicked the login button");
+}
+
+function newUserButtonOnClick() {
+  alert("You clicked the new user button");
+}
+
+function resetpwButtonOnClick() {
+  alert("You clicked the reset pw button");
+}
+
+function dlapkButtonOnClick() {
+  alert("You clicked the dl button");
+}
