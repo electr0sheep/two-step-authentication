3542081e496b910aaa8c1d317a39497779b1fa79
electr0sheep <electr0sheep@electr0sheep.com>
2018-05-01 16:24:44 -0600

    explode worked, testing remaining

--- web/php/new.php
+++ web/php/new.php
@@ -42,20 +42,20 @@
   curl_setopt($ch, CURLOPT_URL, "https://api.pwnedpasswords.com/range/".substr($pwsha1,0,5));
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
   $pwnedResponse = curl_exec($ch);
-  $pwnedResponse = explode(PHP_EOL, $pwnedResponse);
-  error_log(gettype('PWNEDRESPONSE TYPE: '.$pwnedResponse));
-  error_log(count($pwnedResponse));
+  $pwnedResponseAr = explode(PHP_EOL, $pwnedResponse);
+  error_log(gettype('PWNEDRESPONSE TYPE: '.$pwnedResponseAr));
+  error_log(count($pwnedResponseAr));
   if ($pwnedResponse === FALSE) {
     sendResponse('Error adding user: '.curl_error($ch));
   } else {
     error_log('WE ARE AT THE FOREACH');
-    // foreach($pwnedResponse as $value) {
-    //   echo('HERE IS A VALUE: '.$value);
-    //   $ar = explode(':', $value);
-    //   if ($ar[0] == substr($pwsha1, 5)) {
-    //     sendResponse('Unable to create new user, that password has been seen in breaches '.$ar[1].' times!');
-    //   }
-    // }
+    foreach($pwnedResponseAr as $value) {
+      echo('HERE IS A VALUE: '.$value);
+      $ar = explode(':', $value);
+      if ($ar[0] == substr($pwsha1, 5)) {
+        sendResponse('Unable to create new user, that password has been seen in breaches '.$ar[1].' times!');
+      }
+    }
   }
   curl_close($ch);
 
